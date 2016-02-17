2f303dc2bda3c49a753c0470a15c745403367647
TheSparrowHunt <jspar002@gold.ac.uk>
2016-02-17 16:25:51 +0000

    menu control working
    got menu control working, with placeholder graphics

---
+++ Assets/menu/menuscript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuscript : MonoBehaviour {
+	public static int menuposition = 0;
+	// Use this for initialization
+	void Start () {
+		 menuposition = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("up")){
+			if (menuscript.menuposition > 0){
+				menuscript.menuposition -= 1;
+			}
+		}
+
+		if (Input.GetButtonDown("down")){
+			if(menuscript.menuposition < 2){
+				menuscript.menuposition += 1;
+			}
+		}
+			
+		print (menuscript.menuposition);
+	}
+}
---
+++ Assets/menu/startmenuitem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startmenuitem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (menuscript.menuposition == 0) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0.0f, 1.0f);
+			print (menuscript.menuposition);
+		} else {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0.0f, 0.0f);
+		}
+	
+	}
+}
