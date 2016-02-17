1dbe46e5b17d3fbfd7a46126c82ea4c93539440e
TheSparrowHunt <jspar002@gold.ac.uk>
2016-02-17 15:18:59 +0000

    Title Added
    Added the main title with small animation.

---
+++ Assets/ptitlemove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ptitlemove : MonoBehaviour {
+	public float Yloc 			= 0.0f;
+	public float change 		= 0.1f;
+	//public float changeofchange = 0.001f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Yloc > 2 || Yloc < -2) {
+			change = -change;
+		}
+
+		//change += changeofchange;
+		Yloc += change;
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0.0f, Yloc);
+	}
+}
