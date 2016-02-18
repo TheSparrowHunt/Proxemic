2a664a3bc53f332cfd1b2d3ea75eedaba5838457
TheSparrowHunt <jspar002@gold.ac.uk>
2016-02-18 02:35:17 +0000

    Added Arrows to menu
    Added arrows to the menu to denote which menu item is selected,
    animation is there, but no functionality yet.

---
+++ Assets/arrow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class arrow : MonoBehaviour {
+	public float scalar;
+	public float scalarchange;
+
+	// Use this for initialization
+	void Start () {
+		scalar = -1.0f;
+		scalarchange = -1.0f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.localScale.y <= -0.3f || transform.localScale.y >= 0.3f) {
+			scalarchange = -scalarchange;
+		}
+		scalar = scalarchange * Time.deltaTime;
+		transform.localScale += new Vector3 (0, scalar, 0);
+	}
+}
