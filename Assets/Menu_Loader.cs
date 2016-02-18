a663f137e6a56cecc98b1a2cec5700df68fd49be
TheSparrowHunt <jspar002@gold.ac.uk>
2016-02-18 14:35:34 +0000

    Setting values to be moved across the game

--- Assets/Menu_Loader.cs
+++ Assets/Menu_Loader.cs
@@ -8,7 +8,14 @@ public class Menu_Loader : MonoBehaviour {
 	void Start () {
 		testvalue = 0;
 		PlayerPrefs.SetInt("InteractionCounter", 0);
+		PlayerPrefs.SetInt("FailedCounter", 0);
+		PlayerPrefs.SetInt("SuccessfulCounter", 0);
+		PlayerPrefs.SetInt("FriendlyCounter", 0);
+		PlayerPrefs.SetInt("HostileCounter", 0);
+		PlayerPrefs.SetInt("NeutralCounter", 0);
+		PlayerPrefs.SetInt("Mood", 0);
 
+		if (PlayerPrefs.HasKey(
 	}
 	
 	// Update is called once per frame
