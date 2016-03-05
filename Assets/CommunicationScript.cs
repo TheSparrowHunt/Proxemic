da32298de98f8be38a3b487ba1c4ebaa8abf5874
TheSparrowHunt <jspar002@gold.ac.uk>
2016-03-05 23:56:33 +0000

    Implemented the CommsCircle in Tutorial

---
+++ Assets/CommunicationScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CommunicationScript : MonoBehaviour {
+
+	//Vector3 position;
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		player = GameObject.Find ("player(Clone)");
+
+
+		transform.position = player.transform.position;
+
+		/*
+		if (GameObject.Find("player") != null)
+		{
+			player = GameObject.Find ("player");
+
+
+			transform.position = player.transform.position;
+			//transform.position = new Vector3 (player.GetComponent<Transform> ().position);
+		}*/
+	
+	}
+}
