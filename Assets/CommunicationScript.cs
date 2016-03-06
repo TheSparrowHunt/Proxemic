5a9ebd0b4950dcddd8f5b51695702d09c6903401
TheSparrowHunt <jspar002@gold.ac.uk>
2016-03-06 01:21:59 +0000

    Communication circle fully implemented

--- Assets/CommunicationScript.cs
+++ Assets/CommunicationScript.cs
@@ -5,11 +5,22 @@ public class CommunicationScript : MonoBehaviour {
 
 	//Vector3 position;
 	GameObject player;
+	int sizeMod;
 
 	// Use this for initialization
 	void Start () {
-	
+		//DEBUG
+		//PlayerPrefs.SetInt ("SuccessfulCounter", 5);
 
+		if (PlayerPrefs.HasKey ("SuccessfulCounter")) {
+			sizeMod = PlayerPrefs.GetInt ("SuccessfulCounter");
+		} else {
+			sizeMod = 0;
+		}
+		//transforms scale of the CommsCircle based on number of successful interactions
+		transform.localScale = new Vector3 ((0.3f/10.0f) * (10.0f+(sizeMod*3)), (0.3f/10.0f) * (10.0f+(sizeMod*3)), 1);
+
+		//the CircleCollider2D automatically resizes when the transform is changed! Heck yeah Unity!
 
 	}
 	
@@ -17,19 +28,24 @@ public class CommunicationScript : MonoBehaviour {
 	void Update () {
 
 		player = GameObject.Find ("player(Clone)");
-
-
-		transform.position = player.transform.position;
-
-		/*
-		if (GameObject.Find("player") != null)
-		{
-			player = GameObject.Find ("player");
-
-
+		//nullcheck then sets this location to the location of the player
+		//probably should have done with tags, but I wasn't really paying attention when I coded this.
+		if (GameObject.Find ("player(Clone)") != null) {
 			transform.position = player.transform.position;
-			//transform.position = new Vector3 (player.GetComponent<Transform> ().position);
-		}*/
+		}
 	
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		//fill this with every interaction in the game
+		//and have it instantiate the object that makes the conversation/battles
+		if (other.gameObject.tag == "Interactable") {
+			if (other.gameObject.name == "hobo") {
+				//trigger hobo conversation
+				print("Interacted with hobo");
+			}
+
+
+		}
 	}
 }
---
+++ Assets/Playercirclesinitialiser.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playercirclesinitialiser : MonoBehaviour {
+	GameObject commsCircle = null;
+
+	// Use this for initialization
+	void Start () {
+		commsCircle = Instantiate (Resources.Load ("Prefab/CommsCircle"), gameObject.transform.position, Quaternion.identity) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
