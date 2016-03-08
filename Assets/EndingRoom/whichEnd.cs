d3bc47e329f14f5ad466a616312a250f403f61d9
OsmanAbdulMoomin <o.abdulmoomin@gmail.com>
2016-03-08 16:40:40 +0000

    Added files via upload
    It chooses which ending to load based on PlayerPrefs.SuccessfulCounter

---
+++ Assets/EndingRoom/whichEnd.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class whichEnd : MonoBehaviour {
+
+	public int WhatEnding = PlayerPrefs.SuccessfulCounter;
+
+	void Start () {
+
+		if (WhatEnding == 0) {
+
+			SceneManager.LoadScene ("Ending_0");
+
+		}
+
+		if (WhatEnding == 1) {
+			
+			SceneManager.LoadScene ("Ending_1");
+		}
+
+		if (WhatEnding == 2) {
+
+			SceneManager.LoadScene ("Ending_1");
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
