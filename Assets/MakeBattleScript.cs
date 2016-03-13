91369fa6334930349f16be27f67d7bcbb51b2097
TheSparrowHunt <jspar002@gold.ac.uk>
2016-03-13 22:36:15 +0000

    Added Player into the conversation scene

--- Assets/MakeBattleScript.cs
+++ Assets/MakeBattleScript.cs
@@ -4,13 +4,15 @@ using System.Collections;
 public class MakeBattleScript : MonoBehaviour {
 	GameObject backg = null;
 	GameObject otherConverser = null;
+	GameObject protConverser = null;
 
 	// Use this for initialization
 	void Start () {
 		GameObject StateController = GameObject.Find ("StateController");
 		StateController.GetComponent<StateController>().gameState = "Battle";
 		backg = Instantiate (Resources.Load ("Prefab/BattleBackg"), new Vector3(0.0f,0.0f,-1.0f), Quaternion.identity) as GameObject;
-		otherConverser = Instantiate (Resources.Load ("Prefab/hobodrawn_0"), new Vector3(5.0f,0.0f,-2.0f), Quaternion.identity) as GameObject;
+		otherConverser = Instantiate (Resources.Load ("Prefab/hobodrawn_0"), new Vector3(7.0f,0.0f,-2.0f), Quaternion.identity) as GameObject;
+		protConverser = Instantiate (Resources.Load ("Prefab/protdrawn_0"), new Vector3(-7.0f,0.0f,-2.0f), Quaternion.identity) as GameObject;
 	}
 	
 	// Update is called once per frame
