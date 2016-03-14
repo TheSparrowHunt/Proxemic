c511116f39e27b705642651e9f7215c0a7c82076
Rugeon <elliott@studiogossett.co.uk>
2016-03-14 22:12:57 +0000

    Incomplete state of losing
    Began implementing the script so the battle can be declared "lost".
    Struggling to make getComponent work.

--- Assets/GameScript.cs
+++ Assets/GameScript.cs
@@ -7,28 +7,38 @@ public class GameScript : MonoBehaviour {
 	public float timeLeft;
 	public Text timerText;
 	public Text nice;
+	public Text shit;
+//	public GameScript losing;
 
-	void Start(){
+
+	void Start () {
 		UpdateText ();
-
+		//GameObject loserbum = GameObject.Find("loserbum");
 	}
 
-	
 	// Update is called once per frame
 	void FixedUpdate () {
+
 		timeLeft -= Time.deltaTime;
+
+
 		if (timeLeft < 0) {
 			timeLeft = 0;
-	
-		} 
-		if (timeLeft == 0) {
-			timerText.text = "";
-			nice.text = "Nice!";
 		}
-		else {
-			UpdateText ();
+//		if (losing.lost == false) {
+//			shit.text = "Shit.";
+//			timerText.text = "";
+//			nice.text = "";
+//		} else {
+		
+			if (timeLeft == 0) {
+				timerText.text = "";
+				nice.text = "Nice!";
+			} else {
+				UpdateText ();
+			}
 		}
-	}
+//}
 
 	void UpdateText() {
 			timerText.text = "" + Mathf.RoundToInt (timeLeft);
---
+++ Assets/losing.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class losing : MonoBehaviour {
+	public bool lost;
+
+
+	// Use this for initialization
+	void Start () {
+		lost = false;
+	}
+
+	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		//Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
+		if (other.gameObject.CompareTag("Sorry"))
+		{
+			lost = true;
+//			gameObject.SetActive (false);
+		}
+	}
+}
