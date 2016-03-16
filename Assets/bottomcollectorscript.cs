01920c8523be1c0b1d8a2a2b83965f7f9a403c89
TheSparrowHunt <jspar002@gold.ac.uk>
2016-03-16 16:10:28 +0000

    Minor changes on hoboboxes

---
+++ Assets/bottomcollectorscript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class bottomcollectorscript : MonoBehaviour {
+	public float timeLeft;
+	public Text timerText;
+	public Text nice;
+	public Text shit;
+	public bool lost;
+
+	//public gameScript losing;
+
+	void Start () {
+		UpdateText ();
+		lost = false;
+	}
+
+	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		//Check the provided Collider2D parameter other to see if it is tagged "Sorry", if it is...
+		if (other.gameObject.CompareTag ("Sorry")) {
+			//gameObject.SetActive (false);
+			lost = true;
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		timeLeft -= Time.deltaTime;
+		//loserbum = GetComponent<losing>();
+
+		if (timeLeft < 0) {
+			timeLeft = 0;
+		}
+		if (lost == true && timeLeft > 0) {
+			shit.text = "Shit!";
+			timerText.text = "";
+			nice.text = "";
+
+		} else {
+			if (timeLeft == 0) {
+				timerText.text = "";
+				nice.text = "Nice!";
+				shit.text = "";
+			} else {
+				UpdateText ();
+			}
+		}
+	}
+
+
+	void UpdateText () {
+		timerText.text = "" + Mathf.RoundToInt (timeLeft);
+	}
+}
+
