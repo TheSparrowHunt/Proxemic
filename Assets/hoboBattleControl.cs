cf8ffd77d58025b2acd5486232a090b207fdfbf2
TheSparrowHunt <jspar002@gold.ac.uk>
2016-03-16 17:08:16 +0000

    battlecontroller works on realtime

--- Assets/hoboBattleControl.cs
+++ Assets/hoboBattleControl.cs
@@ -3,16 +3,25 @@ using System.Collections;
 
 public class hoboBattleControl : MonoBehaviour {
 	private int counter;
+
 	private GameObject hobotext0;
+	private bool hobotext0generated;
+
+
+	private float timer;
 	// Use this for initialization
 	void Start () {
+		timer = 0.0f;
 		counter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (counter == 30) {
-			hobotext0 = Instantiate (Resources.Load ("Prefab/hoboText/hobotext0"), new Vector3 (1.0f, 4.5f, -3.0f), Quaternion.identity) as GameObject;
+		if (timer >= 0.5f) {
+			if (!hobotext0generated) {
+				hobotext0 = Instantiate (Resources.Load ("Prefab/hoboText/hobotext0"), new Vector3 (1.0f, 4.5f, -3.0f), Quaternion.identity) as GameObject;
+				hobotext0generated = true;
+			}
 		}
 
 
@@ -21,5 +30,6 @@ public class hoboBattleControl : MonoBehaviour {
 
 
 		counter++;
+		timer += Time.deltaTime;
 	}
 }
