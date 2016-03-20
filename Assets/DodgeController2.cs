7dd155354729a91da50083d0fc623f063d1e436b
Rugeon <elliott@studiogossett.co.uk>
2016-03-20 22:21:43 +0000

    Created Second Variation of Dodging Microgame
    Should work fine same as before

---
+++ Assets/DodgeController2.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DodgeController2 : MonoBehaviour {
+
+	public GameObject[] ThingsR;
+	public GameObject[] ThingsL;
+	public float timeLeft;
+
+	public float maxHeight;
+
+	// Use this for initialization
+	void Start () {
+
+		maxHeight = 5.0f;
+		StartCoroutine (Spawn ());
+		StartCoroutine (Spawn2());
+	}
+
+	void FixedUpdate () {
+		timeLeft -= Time.deltaTime;
+
+	}
+
+	IEnumerator Spawn() {
+		yield return new WaitForSeconds (2.0f);
+
+		while(timeLeft > 0.0f) {
+			GameObject ThingR = ThingsR [Random.Range (0, ThingsR.Length)];
+
+			Instantiate (ThingR, (new Vector3 (
+				10,
+				Random.Range (-maxHeight, maxHeight),
+				0
+			)), Quaternion.identity);
+
+			yield return new WaitForSeconds (Random.Range (1.5f, 2.0f));
+		}
+	}
+
+	IEnumerator Spawn2() {
+		yield return new WaitForSeconds (2.0f);
+
+		while(timeLeft > 0.0f) {
+			GameObject ThingL = ThingsL [Random.Range (0, ThingsL.Length)];
+
+			Instantiate (ThingL, (new Vector3 (
+				-10.0f,
+				Random.Range (-maxHeight, maxHeight),
+				0
+			)), Quaternion.identity);
+
+			yield return new WaitForSeconds (Random.Range (1.5f, 2.0f));
+		}
+	}
+}
---
+++ Assets/DodgeInstantiator2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DodgeInstantiator2 : MonoBehaviour {
+	private GameObject Dodgems2;
+
+
+	// Use this for initialization
+	void Start () {
+		Dodgems2 = Instantiate (Resources.Load ("Prefab/Dodgems2/Dodgems2"), new Vector3 (0.0f, 0.0f, 0.0f), Quaternion.identity) as GameObject;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Destroy (gameObject);
+	}
+}
