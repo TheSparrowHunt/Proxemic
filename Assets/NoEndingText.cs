9a560920a844a362daf3fc4ed6ae69e118bebb70
TheSparrowHunt <jspar002@gold.ac.uk>
2016-03-21 06:52:28 +0000

    Minor changes to ending0

--- Assets/NoEndingText.cs
+++ Assets/NoEndingText.cs
@@ -5,10 +5,10 @@ public class NoEndingText : MonoBehaviour {
 	private float timer;
 
 	private GameObject text0;
-//	private bool madeText0 = false;
+	private bool madeText0 = false;
 
 	private GameObject text1;
-//	private bool madeText1 = false;
+	private bool madeText1 = false;
 
 	// Use this for initialization
 	void Start () {
@@ -20,22 +20,18 @@ public class NoEndingText : MonoBehaviour {
 	void Update () {
 
 	if (timer >= 1.0f) {
-//			if (!madeText0) {
+			if (!madeText0) {
 
-				text0 = Instantiate (Resources.Load ("Prefab/endings/noending/Text/noendText0")	)				 
-//					,otherPosition, Quaternion.identity) 
-					as GameObject;
-//
-//				madeText0 = true;
+				text0 = Instantiate (Resources.Load ("Prefab/endings/noending/Text/noendText0"), new Vector3(-8.0f, 4.0f, -1.0f) , Quaternion.identity) as GameObject;
+				madeText0 = true;
 			}
-//		}
+		}
 	if (timer >= 3.0f) {
-//			if (!madeText1){
-				text1 = Instantiate (Resources.Load ("Prefab/endings/noending/Text/noendText1")	)				 
-					//					,otherPosition, Quaternion.identity) 
-					as GameObject;
-//			madeText1 = true;
-//		}
+			if (!madeText1){
+				text1 = Instantiate (Resources.Load ("Prefab/endings/noending/Text/noendText1"), new Vector3(-8.0f, 3.0f, -1.0f) , Quaternion.identity) as GameObject;				 
+
+				madeText1 = true;
+		}
 	}
 	
 		timer += Time.deltaTime;
