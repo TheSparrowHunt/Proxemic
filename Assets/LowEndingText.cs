1377259949fc3f14f5f555e7b733c6fe89004002
Rugeon <elliott@studiogossett.co.uk>
2016-03-21 06:52:40 +0000

    Working on low ending

--- Assets/LowEndingText.cs
+++ Assets/LowEndingText.cs
@@ -1,44 +1,44 @@
-﻿//using UnityEngine;
-//using System.Collections;
-//
-//public class NoEndingText : MonoBehaviour {
-//	private float timer;
-//
-//	private GameObject text0;
-//	private bool madeText0 = false;
-//
-//	private GameObject text1;
-//	private bool madeText1 = false;
-//
-//	// Use this for initialization
-//	void Start () {
-//		timer = 0.0f;
-//	}
-//
-//
-//	// Update is called once per frame
-//	void Update () {
-//
-//		if (timer >= 1.0f) {
-//			if (!madeText0) {
-//
-//				text0 = Instantiate (Resources.Load ("Prefab/endings/noending/Text/noendText0")	)				 
-//					//					,otherPosition, Quaternion.identity) 
-//					as GameObject;
-//
-//				madeText0 = true;
-//			}
-//		}
-//		if (timer >= 3.0f) {
-//			if (!madeText1){
-//				text1 = Instantiate (Resources.Load ("Prefab/endings/noending/Text/noendText1")	)				 
-//					//					,otherPosition, Quaternion.identity) 
-//					as GameObject;
-//				madeText1 = true;
-//			}
-//		}
-//		timer += Time.deltaTime;
-//	}
-//
-//
-//}
+﻿using UnityEngine;
+using System.Collections;
+
+public class LowEndingText : MonoBehaviour {
+	private float timer;
+
+	private GameObject text0;
+	private GameObject text1;
+	private GameObject text2;
+	private GameObject text3;
+	private GameObject text4;
+	private GameObject text5;
+
+
+	// Use this for initialization
+	void Start () {
+		timer = 0.0f;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (timer >= 1.0f) {
+
+
+				text0 = Instantiate (Resources.Load ("Prefab/endings/noending/Text/noendText0")	)				 
+					//					,otherPosition, Quaternion.identity) 
+					as GameObject;
+
+
+		}
+		if (timer >= 3.0f) {
+
+				text1 = Instantiate (Resources.Load ("Prefab/endings/noending/Text/noendText1")	)				 
+					//					,otherPosition, Quaternion.identity) 
+					as GameObject;
+
+		}
+		timer += Time.deltaTime;
+	}
+
+
+}
